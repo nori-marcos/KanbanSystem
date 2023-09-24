1a3f861e99fc657cee9f610a167360ecdee5cd17
Marcos Noriyuki Miyata <marcos.noriyuki.miyata@gmail.com>
2023-09-24 05:51:33 -0300

    Adicionando uma classe de teste, que será excluída

---
+++ Teste.cpp
@@ -0,0 +1,5 @@
+//
+// Created by marcos on 24/09/23.
+//
+
+#include "Teste.h"
---
+++ Teste.h
@@ -0,0 +1,14 @@
+//
+// Created by marcos on 24/09/23.
+//
+
+#ifndef KANBANSYSTEM_TESTE_H
+#define KANBANSYSTEM_TESTE_H
+
+
+class Teste {
+
+};
+
+
+#endif //KANBANSYSTEM_TESTE_H
