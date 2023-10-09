17dee1cdf6f652a21dc577ce2f0e07d6de85f526
LuisHenriqueWToledo <130501067+LuisHenriqueWToledo@users.noreply.github.com>
2023-10-09 13:34:11 -0300

    criação dominio Coluna

--- include/dominios/Coluna.h
+++ include/dominios/Coluna.h
@@ -7,9 +7,33 @@ using namespace std;
 
 class Coluna {
 public:
-    string SOLICITADO = "SOLICITADO";
-    string EM_EXECUCAO = "EM_EXECUCAO";
-    string CONCLUIDO = "CONCLUIDO";
+
+	private:
+	
+	// atributo de "status da coluna" deve ser alterado por outras classes.
+	//strings possiveis: "SOLICITADO" "EM_EXECUCAO" e "CONCLUIDO";
+				
+	public:
+			
+		string status;
+		
+		Coluna(const string &status);
+		
+		Coluna() = default;
+		
+		string getColuna() const
+		{
+			return status;
+		}
+
+    
+		void setStatus(const string &newStatus)
+		{
+			validarStatus(newStatus);
+        	this->status = newStatus;
+    	}
+
+
 };
 
 #endif // COLUNA_H
