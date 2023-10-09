a63acebfd0470b6aafc971a77103bc909ead3897
LuisHenriqueWToledo <130501067+LuisHenriqueWToledo@users.noreply.github.com>
2023-10-09 16:53:08 -0300

    criação dominio Coluna (#2)

--- src/dominios/Coluna.cpp
+++ src/dominios/Coluna.cpp
@@ -1,3 +1,19 @@
 #include "include/dominios/Coluna.h"
 
+//#include "Coluna.h"
 
+
+
+Coluna::Coluna(const string &status) {
+    validarStatus(status);
+    this->status = status;
+}
+
+void Coluna::validarStatus(string int &newStatus)
+ {
+    if ((newStatus != "SOLICITADO") || (newStatus != "EM EXECUÇÃO")
+		(newStauts != "CONCLUIDO")
+		{
+			throw invalid_argument("Digite o status (SOLICITADO, EM EXECUÇÃO, CONCLUIDO) do cartão com caps-lock ativo.");
+    	}
+}
