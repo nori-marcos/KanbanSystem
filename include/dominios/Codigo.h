74e53aed17805bdb5d961c2a26f275397c3fb33c
Marcos Noriyuki Miyata <marcos.noriyuki.miyata@gmail.com>
2023-10-10 23:48:29 -0300

    doxygen

--- include/dominios/Codigo.h
+++ include/dominios/Codigo.h
@@ -6,25 +6,25 @@
 
 using namespace std;
 
+///Classe que representa o código de um cartão.
+///Regras de formato:
+/// - Exemplo: "AB12".
+/// - Deve ser composto por 2 letras maiúsculas e 2 dígitos.
 class Codigo {
 private:
-    ///Atributo que armazena o código
     string codigo;
 
-    ///Valida o código passado por parâmetro, que de ser composto por 2 letras maiúsculas e 2 dígitos.
-    /// @param codigo código a ser validado
     static void validar(string codigo);
 
 public:
     ///Inicializa o objeto com o código passado por parâmetro.
-    ///Regras de formato:
-    /// - Exemplo: "AB12".
-    /// - Deve ser composto por 2 letras maiúsculas e 2 dígitos.
     Codigo(const string &codigo);
 
+    ///Inicializa o objeto com o código vazio.
     Codigo() = default;
 
     ///Atribui um novo código ao objeto
+    ///@param newCodigo código a ser atribuído ao objeto que precisa ser válido
     void setCodigo(const string &newCodigo) {
         validar(newCodigo);
         this->codigo = newCodigo;
